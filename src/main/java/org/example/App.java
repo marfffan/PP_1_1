c2405c7da92d64c2813c4f0bcfb5cf75398e6a13
marfffan <marfffan@gmail.com>
2024-03-29 13:47:49 +0300

    ещё я прикрутил базу данных к проекту в задание это не входит но просто я хотел ещё закоммитить и посмотреть как изменения записываються

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
+import java.sql.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! " );
+    public static void main( String[] args ) throws SQLException {
+        System.out.println( "Попробую соедениться с базой данных " );
+
+        Connection connection  = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/pp_1_1",
+                "root", "1125");
     }
 }
